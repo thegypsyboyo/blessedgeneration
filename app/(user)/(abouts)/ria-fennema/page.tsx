60df765906705da55f0c2b5145f802de2471cf67
thegypsyboyo <lewisonyango9@gmail.com>
2023-09-25 02:30:35 +0300

    These are the changes

--- app/(user)/(abouts)/ria-fennema/page.tsx
+++ app/(user)/(abouts)/ria-fennema/page.tsx
@@ -4,25 +4,56 @@ import { riaQuery } from '@/lib/queries';
 import { sanityFetch } from '@/lib/sanityFetch';
 import urlFor from '@/lib/urlFor';
 import { RiaProps } from '@/typings';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export async function page () {
-  const aboutRia = await sanityFetch<RiaProps[]>({ query: riaQuery });
-  console.log("Ria Query:", aboutRia)
+// export async function page () {
+//   const aboutRia = await sanityFetch<RiaProps[]>({ query: riaQuery });
+//   console.log("Ria Query:", aboutRia)
 
  
 
+//   return (
+//     <>
+//     {aboutRia.map((item, index) => (
+//     <div key={index}>
+//         <Pageheader className='' title={item.name}/>
+//         <Ria aboutRia={aboutRia}/>
+//     </div>
+
+//     ))}
+//     </>
+//   )
+// }
+
+// export default page
+
+
+const RiaPage: React.FC = () => {
+  const [aboutRia, setAboutRia] = useState<RiaProps[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await sanityFetch<RiaProps[]>({ query: riaQuery });
+        setAboutRia(response);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
-    {aboutRia.map((item, index) => (
-    <div key={index}>
-        <Pageheader className='' title={item.name}/>
-        <Ria aboutRia={aboutRia}/>
-    </div>
-
-    ))}
+      {aboutRia.map((item, index) => (
+        <div key={index}>
+          <Pageheader className='' title={item.name} />
+          <Ria aboutRia={aboutRia} />
+        </div>
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default page
+export default RiaPage;
