b37c14d6136075f6fe5adc8bf26c5ba010cf7160
thegypsyboyo <lewisonyango9@gmail.com>
2023-09-25 03:04:06 +0300

    These are the changes

--- app/(user)/(abouts)/ria-fennema/page.tsx
+++ app/(user)/(abouts)/ria-fennema/page.tsx
@@ -1,59 +1,38 @@
-import Ria from '@/components/About/Ria'
-import Pageheader from '@/constants/Pageheader'
+import Ria from '@/components/About/Ria';
+import Pageheader from '@/constants/Pageheader';
 import { riaQuery } from '@/lib/queries';
 import { sanityFetch } from '@/lib/sanityFetch';
-import urlFor from '@/lib/urlFor';
 import { RiaProps } from '@/typings';
-import React, { useEffect, useState } from 'react'
+import React from 'react';
 
-// export async function page () {
-//   const aboutRia = await sanityFetch<RiaProps[]>({ query: riaQuery });
-//   console.log("Ria Query:", aboutRia)
+export async function page() {
+  const fetchData = async () => {
+    try {
+      const aboutRia = await sanityFetch<RiaProps[]>({ query: riaQuery });
+      console.log("Ria Query:", aboutRia);
+      return aboutRia;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      return []; // Return an empty array in case of an error
+    }
+  };
 
- 
-
-//   return (
-//     <>
-//     {aboutRia.map((item, index) => (
-//     <div key={index}>
-//         <Pageheader className='' title={item.name}/>
-//         <Ria aboutRia={aboutRia}/>
-//     </div>
-
-//     ))}
-//     </>
-//   )
-// }
-
-// export default page
-
-
-const RiaPage: React.FC = () => {
-  const [aboutRia, setAboutRia] = useState<RiaProps[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await sanityFetch<RiaProps[]>({ query: riaQuery });
-        setAboutRia(response);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
+  const aboutRiaData = fetchData();
 
   return (
     <>
-      {aboutRia.map((item, index) => (
-        <div key={index}>
-          <Pageheader className='' title={item.name} />
-          <Ria aboutRia={aboutRia} />
-        </div>
+      {aboutRiaData.then((aboutRia) => (
+        <>
+          {aboutRia.map((item, index) => (
+            <div key={index}>
+              <Pageheader className='' title={item.name} />
+              <Ria aboutRia={aboutRia} />
+            </div>
+          ))}
+        </>
       ))}
     </>
   );
-};
+}
 
-export default RiaPage;
+export default page;
