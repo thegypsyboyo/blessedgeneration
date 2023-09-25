d6337b5fb0d6c976d72f2389b0d8078fcc6f4fae
thegypsyboyo <lewisonyango9@gmail.com>
2023-09-25 03:35:30 +0300

    These are the changes

--- app/(user)/(abouts)/ria-fennema/page.tsx
+++ app/(user)/(abouts)/ria-fennema/page.tsx
@@ -1,38 +1,54 @@
-import Ria from '@/components/About/Ria';
-import Pageheader from '@/constants/Pageheader';
+import Ria from '@/components/About/Ria'
+import Pageheader from '@/constants/Pageheader'
 import { riaQuery } from '@/lib/queries';
 import { sanityFetch } from '@/lib/sanityFetch';
+import urlFor from '@/lib/urlFor';
 import { RiaProps } from '@/typings';
-import React from 'react';
+import React from 'react'
 
-export async function page() {
+// export async function page () {
+//   const aboutRia = await sanityFetch<RiaProps[]>({ query: riaQuery });
+//   console.log("Ria Query:", aboutRia)
+
+ 
+
+//   return (
+//     <>
+//     {aboutRia.map((item, index) => (
+//     <div key={index}>
+//         <Pageheader className='' title={item.name}/>
+//         <Ria aboutRia={aboutRia}/>
+//     </div>
+
+//     ))}
+//     </>
+//   )
+// }
+
+// export default page
+
+const Page: React.FC = () => {
   const fetchData = async () => {
-    try {
-      const aboutRia = await sanityFetch<RiaProps[]>({ query: riaQuery });
-      console.log("Ria Query:", aboutRia);
-      return aboutRia;
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      return []; // Return an empty array in case of an error
-    }
+    const aboutRia = await sanityFetch<RiaProps[]>({ query: riaQuery });
+    return aboutRia;
   };
 
-  const aboutRiaData = fetchData();
+  const aboutBgData = fetchData();
 
   return (
     <>
-      {aboutRiaData.then((aboutRia) => (
+      {aboutBgData.then((aboutBg) => (
         <>
-          {aboutRia.map((item, index) => (
+          {aboutBg.map((item, index) => (
             <div key={index}>
               <Pageheader className='' title={item.name} />
-              <Ria aboutRia={aboutRia} />
+              <Ria aboutRia={aboutBg} />
             </div>
           ))}
         </>
       ))}
     </>
   );
-}
+};
 
-export default page;
+export default Page;
